4714e4a5411ccbb94e7c05ddbce8efb1ad7e15aa
Cagil Soylu <cagil30@live.de>
2026-06-06 10:47:52 +0200

    add linear control-lfow basic target and project restructuring

---
+++ targets/basic01_linear/unprotected.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+/*
+ * This target models a simple multi-step security decision.
+ * The operation is only considered valid if all required
+ * cehcks are executed in sequenc.
+ * 
+ * Security interpretation:
+ *  - step1: firmware-header was parsed
+ *  - step2: firmware version was checked 
+ *  - step3: security-policy was checked 
+ * 
+ * Fault Model:
+ *  if an instruction skip removes one of the linear steps,
+ *  the final progress valued differes from the expected value, 
+ *  and the target returns false.
+ * 
+ * Relevance:
+ *  This is the baseline case for evaluating whether the 
+ *  countermeasure can protect sequential execution without 
+ *  branches, merges, function calls or loops.
+ * 
+*/
+
+
+volatile uint32_t value = 0;
+
+bool linear_target(void)
+{
+    int checks_passed = 0;
+
+    // Fault on
+    checks_passed++;
+    checks_passed++;
+    checks_passed++;
+
+    value = checks_passed;
+
+    // The operation is accepted only if all linear steps ran.
+    return (value == 3);
+}
